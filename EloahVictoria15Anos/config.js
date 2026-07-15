ac7709c1bd4f9def03c20b21bd298a2a34dcd807
Builder v8 <builder@mforge.com.br>
2026-07-15 17:31:16 -0300

    Deploy convite: EloahVictoria15Anos

--- EloahVictoria15Anos/config.js
+++ EloahVictoria15Anos/config.js
@@ -14,7 +14,7 @@ window.config = {
   "convite": {
     "paletaCores": "Rosê",
     "tema": "Floral",
-    "musica": "assets/music_1784147363342.mp3",
+    "musica": "assets/music_1784147475702.mp3",
     "tipoAbertura": "longa",
     "particulasAbertura": true,
     "slug": "EloahVictoria15Anos",
@@ -25,33 +25,33 @@ window.config = {
     }
   },
   "assets": {
-    "capa": "assets/cover_1784147363342.png",
-    "cartao": "assets/cartao_chatgpt_1784147363342.jpg",
+    "capa": "assets/cover_1784147475702.png",
+    "cartao": "assets/cartao_chatgpt_1784147475702.jpg",
     "aberturaSlides": [
-      "assets/slide1_1784147363342.mp4",
-      "assets/slide2_1784147363342.jpg",
-      "assets/slide3_1784147363342.jpg"
+      "assets/slide1_1784147475702.mp4",
+      "assets/slide2_1784147475702.jpg",
+      "assets/slide3_1784147475702.jpg"
     ],
     "aberturaSlidesMeta": [
       {
-        "filePath": "assets/slide1_1784147363342.mp4",
+        "filePath": "assets/slide1_1784147475702.mp4",
         "label": "Abertura (Video)",
         "type": "video"
       },
       {
-        "filePath": "assets/slide2_1784147363342.jpg",
+        "filePath": "assets/slide2_1784147475702.jpg",
         "label": "Convite Especial",
         "type": "image"
       },
       {
-        "filePath": "assets/slide3_1784147363342.jpg",
+        "filePath": "assets/slide3_1784147475702.jpg",
         "label": "Prepare-se",
         "type": "image"
       }
     ],
-    "folhaVazia": "assets/folha_vazia_1784147363342.png",
-    "folhaPreenchida": "assets/folha_1784147363342.jpg",
-    "musica": "assets/music_1784147363342.mp3",
+    "folhaVazia": "assets/folha_vazia_1784147475702.png",
+    "folhaPreenchida": "assets/folha_1784147475702.jpg",
+    "musica": "assets/music_1784147475702.mp3",
     "fabric": {
       "saveTheDate": {
         "referencia": "",
@@ -81,7 +81,7 @@ window.config = {
       "tipoVisual": "css",
       "titulo": "Como Chegar",
       "icone": "fa-solid fa-location-dot",
-      "conteudo": "link pendente"
+      "conteudo": "https://maps.app.goo.gl/pUv1c1YFzZLfx8Jq7"
     },
     {
       "tipoAcao": "RSVP",
