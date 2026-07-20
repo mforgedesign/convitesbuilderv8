ca31c4c8bb0541cf09d25c4c51471a726a7e12df
Builder v8 <builder@mforge.com.br>
2026-07-20 18:25:31 -0300

    Deploy convite: Manuella-15Anos

--- Manuella-15Anos/config.js
+++ Manuella-15Anos/config.js
@@ -14,7 +14,7 @@ window.config = {
   "convite": {
     "paletaCores": "Azul Marinho com Prata",
     "tema": "Céu Estrelado",
-    "musica": "assets/music_1784580373151.mp3",
+    "musica": "assets/music_1784582730775.mp3",
     "tipoAbertura": "longa",
     "particulasAbertura": true,
     "slug": "Manuella-15Anos",
@@ -25,33 +25,33 @@ window.config = {
     }
   },
   "assets": {
-    "capa": "assets/cover_1784580373151.png",
-    "cartao": "assets/cartao_chatgpt_1784580373151.jpg",
+    "capa": "assets/cover_1784582730775.png",
+    "cartao": "assets/cartao_chatgpt_1784582730775.jpg",
     "aberturaSlides": [
-      "assets/slide1_1784580373151.mp4",
-      "assets/slide2_1784580373151.mp4",
-      "assets/slide3_1784580373151.mp4"
+      "assets/slide1_1784582730775.mp4",
+      "assets/slide2_1784582730775.mp4",
+      "assets/slide3_1784582730775.mp4"
     ],
     "aberturaSlidesMeta": [
       {
-        "filePath": "assets/slide1_1784580373151.mp4",
+        "filePath": "assets/slide1_1784582730775.mp4",
         "label": "Abertura (Vídeo)",
         "type": "video"
       },
       {
-        "filePath": "assets/slide2_1784580373151.mp4",
+        "filePath": "assets/slide2_1784582730775.mp4",
         "label": "Slide 1",
         "type": "video"
       },
       {
-        "filePath": "assets/slide3_1784580373151.mp4",
+        "filePath": "assets/slide3_1784582730775.mp4",
         "label": "Slide 2",
         "type": "video"
       }
     ],
     "folhaVazia": "",
-    "folhaPreenchida": "assets/folha_1784580373151.png",
-    "musica": "assets/music_1784580373151.mp3",
+    "folhaPreenchida": "assets/folha_1784582730775.png",
+    "musica": "assets/music_1784582730775.mp3",
     "fabric": {
       "saveTheDate": {
         "referencia": "",
@@ -81,7 +81,7 @@ window.config = {
       "tipoVisual": "css",
       "titulo": "Como Chegar",
       "icone": "fa-solid fa-location-dot",
-      "conteudo": "https://maps.app.goo.gl/fFQJizVi2F9Hyhp6A"
+      "conteudo": "https://maps.app.goo.gl/cXsVjdmqfHuuERxh9"
     },
     {
       "tipoAcao": "PopupHtml",
@@ -102,7 +102,7 @@ window.config = {
       "tipoVisual": "css",
       "titulo": "Confirmar Presença",
       "icone": "fa-solid fa-check",
-      "whatsapp": "5511967140092",
+      "whatsapp": "5511989211567",
       "rsvpConfig": {
         "exibirNome": true,
         "exibirWhatsapp": false,
